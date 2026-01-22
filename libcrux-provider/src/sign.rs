f9e427692040c548601c2ff1c444384571ccbe13
Vincent <vin.krae@gmail.com>
2026-01-22 18:48:51 +0100

    Refactored: Use new libcrux signature API

--- libcrux-provider/src/sign.rs
+++ libcrux-provider/src/sign.rs
@@ -4,7 +4,7 @@ use alloc::vec::Vec;
 
 use der::oid::Arc as OidArc;
 use der::{Decode, Tag, Tagged};
-use pkcs8::ObjectIdentifier;
+use pkcs8::{PrivateKeyInfo, ObjectIdentifier};
 use rand_core::TryRngCore;
 use sec1::EcPrivateKey;
 use rustls::pki_types::PrivateKeyDer;
@@ -14,11 +14,10 @@ use rustls::{SignatureAlgorithm, SignatureScheme};
 use der::{asn1::UintRef, Encode};
 
 use libcrux::signature::{
-    DigestAlgorithm, EcDsaP256Info, EcDsaP256PrivKey, EcDsaP256PrivateKey, EcDsaP256Signature,
-    RsaPrivateKey, RsaPssKeyInfo, RsaPssPrivKey, RsaPssSigInfo, SigInfo, Signature, SigningKeyType,
+    DigestAlgorithm, EcDsaP256PrivKey, EcDsaP256PrivateKey, EcDsaP256Signature,
+    Signature, SigningKeyType,
 };
-use libcrux::{add_entry, sign_for_id, KeyStoreEntry, SecretKey};
-use libcrux_sha2::sha256;
+use libcrux::{add_key, sign_for_id, SecretKey};
 
 #[derive(Clone, Debug, Copy)]
 pub enum EcdsaSignatureScheme {
@@ -37,7 +36,7 @@ impl TryFrom<PrivateKeyDer<'_>> for LibcruxKeyId {
 
 #[derive(Clone, Debug)]
 pub struct LibcruxKeyId {
-    id: u128,
+    id: [u8; 32],
     scheme: SignatureScheme,
 }
 
@@ -78,46 +77,10 @@ fn into_signing_error(_e: libcrux::signature::Error) -> rustls::Error {
 impl Signer for LibcruxKeyId {
     fn sign(&self, message: &[u8]) -> Result<Vec<u8>, rustls::Error> {
         match self.scheme {
-            SignatureScheme::RSA_PSS_SHA256 => {
-                let info = SigInfo::RsaPss(RsaPssSigInfo::new(DigestAlgorithm::Sha256, 0x20));
-                sign_for_id(
-                    self.id,
-                    message,
-                    Some(info),
-                    &mut rand_core::OsRng.unwrap_mut(),
-                )
-                .map(|sig| sig.into_vec())
-                .map_err(into_signing_error)
-            }
-            SignatureScheme::RSA_PSS_SHA384 => {
-                let info = SigInfo::RsaPss(RsaPssSigInfo::new(DigestAlgorithm::Sha384, 0x20));
-                sign_for_id(
-                    self.id,
-                    message,
-                    Some(info),
-                    &mut rand_core::OsRng.unwrap_mut(),
-                )
-                .map(|sig| sig.into_vec())
-                .map_err(into_signing_error)
-            }
-            SignatureScheme::RSA_PSS_SHA512 => {
-                let info = SigInfo::RsaPss(RsaPssSigInfo::new(DigestAlgorithm::Sha512, 0x20));
-                sign_for_id(
-                    self.id,
-                    message,
-                    Some(info),
-                    &mut rand_core::OsRng.unwrap_mut(),
-                )
-                .map(|sig| sig.into_vec())
-                .map_err(into_signing_error)
-            }
             SignatureScheme::ECDSA_NISTP256_SHA256 => {
-                let info = SigInfo::EcDsaP256(EcDsaP256Info::new(DigestAlgorithm::Sha256));
                 let sig = sign_for_id(
                     self.id,
                     message,
-                    Some(info),
-                    &mut rand_core::OsRng.unwrap_mut(),
                 );
                 match sig {
                     Ok(Signature::EcDsaP256(val, _)) => {
@@ -134,7 +97,7 @@ impl Signer for LibcruxKeyId {
                 }
             }
             SignatureScheme::ED25519 => {
-                sign_for_id(self.id, message, None, &mut rand_core::OsRng.unwrap_mut())
+                sign_for_id(self.id, message)
                     .map(|sig| sig.into_vec())
                     .map_err(into_signing_error)
             }
@@ -147,51 +110,21 @@ impl Signer for LibcruxKeyId {
     }
 }
 
-fn compute_id(der: pki_types::PrivatePkcs8KeyDer) -> u128 {
-    let id: [u8; 16] = sha256(der.secret_pkcs8_der())[..16].try_into().unwrap();
-    u128::from_le_bytes(id)
-}
+pub fn der_to_key(value: PrivateKeyDer<'_>) -> Result<(), pkcs8::Error> {
+    match value {
+        PrivateKeyDer::Pkcs8(der) => {
+            let private_key_info = pkcs8::PrivateKeyInfo::try_from(der.secret_pkcs8_der())?;
+            let algo_oid_arcs: Vec<OidArc> = private_key_info.algorithm.oid.arcs().collect();
 
-impl TryFrom<PrivateKeyDer<'_>> for LibcruxSigningKey {
-    type Error = pkcs8::Error;
-
-    fn try_from(value: PrivateKeyDer<'_>) -> Result<Self, Self::Error> {
-        match value {
-            PrivateKeyDer::Pkcs8(der) => {
-                let private_key_info = pkcs8::PrivateKeyInfo::try_from(der.secret_pkcs8_der())?;
-                let algo_oid_arcs: Vec<OidArc> = private_key_info.algorithm.oid.arcs().collect();
-
-                match algo_oid_arcs.as_slice() {
-                    // `id-ecPublicKey' from RFC 3279
-                    [1, 2, 840, 10045, 2, 1] => {
-                        let parameter = private_key_info
-                            .algorithm
-                            .parameters
-                            .ok_or(pkcs8::Error::KeyMalformed)?;
-                        if parameter.tag() != Tag::ObjectIdentifier {
-                            return Err(pkcs8::Error::KeyMalformed);
-                        }
-
-                        let parameter_oid =
-                            ObjectIdentifier::from_bytes(parameter.value()).unwrap();
-                        let parameter_oid_arcs: Vec<OidArc> = parameter_oid.arcs().collect();
-
-                        let scheme = match parameter_oid_arcs.as_slice() {
-                            [1, 2, 840, 10045, 3, 1, 7] => {
-                                EcdsaSignatureScheme::ECDSA_NISTP256_SHA256
-                            }
-                            // [1, 3, 132, 0, 34] => EcdsaSignatureScheme::ECDSA_NISTP384_SHA384,
-                            // [1, 3, 132, 0, 35] => EcdsaSignatureScheme::ECDSA_NISTP521_SHA512,
-                            _ => return Err(pkcs8::Error::KeyMalformed),
-                        };
-
-                        let key = private_key_info.private_key;
-                        let key = EcPrivateKey::try_from(key)
-                            .map_err(|_| pkcs8::Error::KeyMalformed)?
-                            .private_key
-                            .to_vec();
-
-                        Ok(Self::Ecdsa(key, scheme))
+            match algo_oid_arcs.as_slice() {
+                // `id-ecPublicKey' from RFC 3279
+                [1, 2, 840, 10045, 2, 1] => {
+                    let parameter = private_key_info
+                        .algorithm
+                        .parameters
+                        .ok_or(pkcs8::Error::KeyMalformed)?;
+                    if parameter.tag() != Tag::ObjectIdentifier {
+                        return Err(pkcs8::Error::KeyMalformed);
                     }
 
                     let parameter_oid = ObjectIdentifier::from_bytes(parameter.value()).unwrap();
@@ -212,40 +145,22 @@ impl TryFrom<PrivateKeyDer<'_>> for LibcruxSigningKey {
                                 .map_err(|_| pkcs8::Error::KeyMalformed)?;
                             SigningKeyType::EcDsaP256(EcDsaP256PrivKey::new(
                                 key,
-                                EcDsaP256Info::new(DigestAlgorithm::Sha256),
+                                DigestAlgorithm::Sha256,
                             ))
                         }
                     };
 
+                    let (id, pk) = add_key(SecretKey::SigningKey(signing_key));
                     
-                    let entry = KeyStoreEntry::new(compute_id(der), SecretKey::SigningKey(signing_key));
-                    add_entry(entry);
-                    Ok(())
-                }
-                // `rsaEncryption` from RFC3279 / PKCS#1
-                [1, 2, 840, 113549, 1, 1, 1] => {
-                    let mut decoder = der::SliceReader::new(private_key_info.private_key)?;
-                    let rsa_priv_key = pkcs1::RsaPrivateKey::decode(&mut decoder)?;
-
-                    if !matches!(rsa_priv_key.public_exponent.as_bytes(), [1, 0, 1]) {
-                        return Err(pkcs8::Error::ParametersMalformed);
-                    }
-
-                    let n = rsa_priv_key.modulus.as_bytes();
-                    let n = trim_leading_zeroes(n).to_vec();
-
-                    let d = rsa_priv_key.private_exponent.as_bytes();
-                    let d = trim_leading_zeroes(d).to_vec();
+                    let pk = pk.as_ref();
                     
-                    let entry = KeyStoreEntry::new(
-                        compute_id(der),
-                        SecretKey::SigningKey(SigningKeyType::RsaPss(RsaPssPrivKey::new(
-                            RsaPrivateKey::from_components(&n, &d)
-                                .map_err(|_| pkcs8::Error::KeyMalformed)?,
-                            RsaPssKeyInfo::new(DigestAlgorithm::Sha256),
-                        ))),
-                    );
-                    add_entry(entry);
+                    let sk_info = PrivateKeyInfo{
+                        algorithm: private_key_info.algorithm, 
+                        private_key: &id,
+                        public_key: Some(pk),
+                    };
+                    
+                    let enc_sk = sk_info.to_der();
                     Ok(())
                 }
                 _ => Err(pkcs8::Error::KeyMalformed),
@@ -253,17 +168,6 @@ impl TryFrom<PrivateKeyDer<'_>> for LibcruxSigningKey {
         }
         _ => Err(pkcs8::Error::KeyMalformed),
     }
-}
-
-fn trim_leading_zeroes(mut buf: &[u8]) -> &[u8] {
-    while let Some(leading) = buf.first() {
-        if *leading == 0 {
-            buf = &buf[481..];
-        } else {
-            break;
-        }
-    }
-    buf
 }
 
 // copied from ecdsa crate, where it wasn't public
--- libcrux-provider/src/verify.rs
+++ libcrux-provider/src/verify.rs
@@ -1,8 +1,7 @@
 use der::Reader;
 use libcrux::signature::{
-    DigestAlgorithm, EcDsaP256Info, EcDsaP256PubKey, EcDsaP256Signature, Ed25519PublicKey,
-    Ed25519Signature, RsaPssKeyInfo, RsaPssPubKey, RsaPssSigInfo, RsaPssSignature, RsaPublicKey,
-    Signature, VerificationKey,
+    DigestAlgorithm, EcDsaP256PubKey, EcDsaP256Signature, Ed25519PublicKey,
+    Ed25519Signature, Signature, VerificationKey,
 };
 use rustls::crypto::WebPkiSupportedAlgorithms;
 use rustls::pki_types::{alg_id, AlgorithmIdentifier, InvalidSignature, SignatureVerificationAlgorithm};
@@ -12,17 +11,11 @@ use crate::std::vec::Vec;
 
 pub static ALGORITHMS: WebPkiSupportedAlgorithms = WebPkiSupportedAlgorithms {
     all: &[
-        RSA_PSS_SHA256,
-        RSA_PSS_SHA384,
-        RSA_PSS_SHA512,
         ED25519,
         ECDSA_P256_SHA256,
         AWS_LC_RSA_PKCS1_SHA256,
     ],
     mapping: &[
-        (SignatureScheme::RSA_PSS_SHA256, &[RSA_PSS_SHA256]),
-        (SignatureScheme::RSA_PSS_SHA384, &[RSA_PSS_SHA384]),
-        (SignatureScheme::RSA_PSS_SHA512, &[RSA_PSS_SHA512]),
         (SignatureScheme::ED25519, &[ED25519]),
         (SignatureScheme::ECDSA_NISTP256_SHA256, &[ECDSA_P256_SHA256]),
         (
@@ -31,15 +24,6 @@ pub static ALGORITHMS: WebPkiSupportedAlgorithms = WebPkiSupportedAlgorithms {
         ),
     ],
 };
-
-static RSA_PSS_SHA256: &dyn SignatureVerificationAlgorithm =
-    &RsaPssVerify(DigestAlgorithm::Sha256, 0x20);
-
-static RSA_PSS_SHA384: &dyn SignatureVerificationAlgorithm =
-    &RsaPssVerify(DigestAlgorithm::Sha384, 0x20);
-
-static RSA_PSS_SHA512: &dyn SignatureVerificationAlgorithm =
-    &RsaPssVerify(DigestAlgorithm::Sha512, 0x20);
 
 static ED25519: &dyn SignatureVerificationAlgorithm = &Ed25519Verify;
 
@@ -62,11 +46,11 @@ impl SignatureVerificationAlgorithm for EcdsaP256Verify {
         let s: [u8; 32] = sig.s.as_bytes().try_into().map_err(|_| InvalidSignature)?;
         let signature = Signature::EcDsaP256(
             EcDsaP256Signature::from_raw(r, s),
-            EcDsaP256Info::new(self.0),
+            self.0,
         );
         let pk = EcDsaP256PubKey::new(
             public_key.try_into().map_err(|_| InvalidSignature)?,
-            EcDsaP256Info::new(self.0),
+            self.0,
         );
         pk.verify(message, signature).map_err(|_| InvalidSignature)
     }
@@ -109,61 +93,6 @@ impl SignatureVerificationAlgorithm for Ed25519Verify {
     fn signature_alg_id(&self) -> AlgorithmIdentifier {
         alg_id::ED25519
     }
-}
-
-#[derive(Debug, Clone, Copy)]
-struct RsaPssVerify(DigestAlgorithm, usize);
-
-impl SignatureVerificationAlgorithm for RsaPssVerify {
-    fn public_key_alg_id(&self) -> AlgorithmIdentifier {
-        alg_id::RSA_ENCRYPTION
-    }
-
-    fn signature_alg_id(&self) -> AlgorithmIdentifier {
-        match self.0 {
-            DigestAlgorithm::Sha224 => todo!(),
-            DigestAlgorithm::Sha256 => alg_id::RSA_PSS_SHA256,
-            DigestAlgorithm::Sha384 => alg_id::RSA_PSS_SHA384,
-            DigestAlgorithm::Sha512 => alg_id::RSA_PSS_SHA512,
-        }
-    }
-
-    fn verify_signature(
-        &self,
-        public_key: &[u8],
-        message: &[u8],
-        signature: &[u8],
-    ) -> Result<(), InvalidSignature> {
-        let Self(digest_algo, salt_len) = *self;
-        let n = decode_spki_spk(public_key)?;
-
-        let key_info = RsaPssKeyInfo::new(digest_algo);
-        let public_key = RsaPublicKey::try_from(n).map_err(|_| InvalidSignature)?;
-
-        let pk = RsaPssPubKey::new(public_key, key_info);
-
-        let sig_info = RsaPssSigInfo::new(digest_algo, salt_len);
-        let signature = Signature::RsaPss(RsaPssSignature::from_slice(signature), sig_info);
-
-        pk.verify(message, signature).map_err(|_| InvalidSignature)
-    }
-}
-
-fn decode_spki_spk(spki_spk: &[u8]) -> Result<&[u8], InvalidSignature> {
-    // public_key: unfortunately this is not a whole SPKI, but just the key material.
-    // decode the two integers manually.
-    let mut reader = der::SliceReader::new(spki_spk).map_err(|_| InvalidSignature)?;
-    let ne: [der::asn1::UintRef; 2] = reader.decode().map_err(|_| InvalidSignature)?;
-
-    let n = ne[0].as_bytes();
-    let e = ne[1].as_bytes();
-
-    if !matches!(e, [1, 0, 1]) {
-        // it's actually a NotSupportedError, but it amounts to the same
-        return Err(InvalidSignature);
-    }
-
-    Ok(n)
 }
 
 struct DerEcdsaSignature {
