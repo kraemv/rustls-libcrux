e090ad88ad14537a0c55a51ab1564f9b3f991b60
Vincent <vin.krae@gmail.com>
2026-04-20 10:26:09 +0200

    Changed: KX over KEM and NIKE API

--- libcrux-provider/src/kx.rs
+++ libcrux-provider/src/kx.rs
@@ -3,21 +3,31 @@ use alloc::string::String;
 
 use rustls::crypto;
 // use crate::pq::X25519MlKem768;
-use libcrux::libcrux::kem::{self as libcrux_api, DecapsKey};
+use libcrux::libcrux::kem;
+use libcrux::libcrux::kem::DecapsKey;
+use libcrux::libcrux::nike;
+use libcrux::libcrux::nike::NIKESecretKey;
+
 
 
 #[derive(Debug)]
-pub struct KeyExchange {
-    priv_key: libcrux_api::DecapsKeyID,
-    pub_key: libcrux_api::EncapsKeyType,
+pub struct KemKeyExchange {
+    priv_key: kem::DecapsKeyID,
+    pub_key: kem::EncapsKeyType,
 }
 
-impl crypto::ActiveKeyExchange for KeyExchange {
+#[derive(Debug)]
+pub struct Nike {
+    priv_key: nike::NIKESecretKeyID,
+    pub_key: nike::NIKEPublicKeyType,
+}
+
+impl crypto::ActiveKeyExchange for KemKeyExchange {
     fn complete(
-        self: Box<KeyExchange>,
+        self: Box<KemKeyExchange>,
         peer: &[u8],
     ) -> Result<crypto::SharedSecret, rustls::Error> {
-        let ct = libcrux_api::EncapsulatedKey::new(self.priv_key.scheme(), peer).map_err(|_| rustls::Error::General(String::from("ecdh derive error")))?;
+        let ct = kem::EncapsulatedKey::new(self.priv_key.scheme(), peer).map_err(|_| rustls::Error::General(String::from("ecdh derive error")))?;
         let shared_secret = self.priv_key.decaps(ct).map_err(|_| rustls::Error::General(String::from("ecdh derive error")))?;
 
         Ok(crypto::SharedSecret::from(&shared_secret[..]))
@@ -29,8 +39,29 @@ impl crypto::ActiveKeyExchange for KeyExchange {
 
     fn group(&self) -> rustls::NamedGroup {
         match self.priv_key.scheme() {
-            libcrux_api::KemScheme::MlKem768 => rustls::NamedGroup::MLKEM768,
-            libcrux_api::KemScheme::X25519 => rustls::NamedGroup::X25519,
+            kem::KemScheme::MlKem768 => rustls::NamedGroup::MLKEM768,
+        }
+    }
+}
+
+impl crypto::ActiveKeyExchange for Nike {
+    fn complete(
+        self: Box<Nike>,
+        peer: &[u8],
+    ) -> Result<crypto::SharedSecret, rustls::Error> {
+        let pk = nike::NIKEPublicKeyType::new(self.priv_key.scheme(), peer).map_err(|_| rustls::Error::General(String::from("ecdh derive error")))?;
+        let shared_secret = self.priv_key.derive(pk).map_err(|_| rustls::Error::General(String::from("ecdh derive error")))?;
+
+        Ok(crypto::SharedSecret::from(&shared_secret[..]))
+    }
+
+    fn pub_key(&self) -> &[u8] {
+        self.pub_key.to_bytes()
+    }
+
+    fn group(&self) -> rustls::NamedGroup {
+        match self.priv_key.scheme() {
+            nike::NIKEScheme::X25519 => rustls::NamedGroup::X25519,
         }
     }
 }
@@ -48,9 +79,9 @@ pub struct MLKEM768;
 
 impl crypto::SupportedKxGroup for X25519 {
     fn start(&self) -> Result<Box<dyn crypto::ActiveKeyExchange>, rustls::Error> {
-        let (priv_key, pub_key) = libcrux_api::DecapsKeyID::gen_x25519_key().map_err(|_| rustls::Error::General(String::from("ecdh keygen error")))?;
+        let (priv_key, pub_key) = nike::NIKESecretKeyID::gen_x25519_key().map_err(|_| rustls::Error::General(String::from("ecdh keygen error")))?;
 
-        Ok(Box::new(KeyExchange { priv_key, pub_key }))
+        Ok(Box::new(Nike { priv_key, pub_key }))
     }
 
     fn name(&self) -> rustls::NamedGroup {
@@ -60,9 +91,9 @@ impl crypto::SupportedKxGroup for X25519 {
 
 impl crypto::SupportedKxGroup for MLKEM768 {
     fn start(&self) -> Result<Box<dyn crypto::ActiveKeyExchange>, rustls::Error> {
-        let (priv_key, pub_key) = libcrux_api::DecapsKeyID::gen_mlkem_768_key().map_err(|_| rustls::Error::General(String::from("ecdh keygen error")))?;
+        let (priv_key, pub_key) = kem::DecapsKeyID::gen_mlkem_768_key().map_err(|_| rustls::Error::General(String::from("ecdh keygen error")))?;
 
-        Ok(Box::new(KeyExchange { priv_key, pub_key }))
+        Ok(Box::new(KemKeyExchange { priv_key, pub_key }))
     }
 
     fn name(&self) -> rustls::NamedGroup {
