1720497a2bb2baf36096d3dafcc10d1040f4b866
Vincent <vin.krae@gmail.com>
2026-05-04 18:16:13 +0200

    Added: HKDF implementation

--- libcrux-provider/src/hmac.rs
+++ libcrux-provider/src/hmac.rs
@@ -3,8 +3,44 @@ use alloc::boxed::Box;
 use rustls::crypto;
 use libcrux::algorithms::sha2 as sha2;
 use libcrux::algorithms::hmac as hmac;
+use libcrux::algorithms::hkdf as hkdf;
 
 pub struct Sha256Hmac;
+pub struct Sha256HKDF;
+
+const SHA2_256_LEN: usize = 32;
+
+impl crypto::tls13::Hkdf for Sha256HKDF {
+    fn extract_from_zero_ikm(&self, salt: Option<&[u8]>) -> Box<dyn crypto::tls13::HkdfExpander> {
+        let mut prk = [0u8; SHA2_256_LEN];
+        let ikm = [0u8; SHA2_256_LEN];
+        let salt = salt.unwrap_or(&[0u8; SHA2_256_LEN]);
+
+        hkdf::sha2_256::extract(&mut prk, salt, &ikm).unwrap();
+
+        Box::new(Sha256HKDFKey(prk))
+    }
+
+    fn extract_from_secret(&self, salt: Option<&[u8]>, secret: &[u8]) -> Box<dyn crypto::tls13::HkdfExpander> {
+        let mut prk = [0u8; SHA2_256_LEN];
+        let salt = salt.unwrap_or(&[0u8; SHA2_256_LEN]);
+
+        hkdf::sha2_256::extract(&mut prk, salt, secret).unwrap();
+
+        Box::new(Sha256HKDFKey(prk))
+    }
+
+    fn expander_for_okm(&self, okm: &crypto::tls13::OkmBlock) -> Box<dyn crypto::tls13::HkdfExpander> {
+        let key: [u8; 32] = okm.as_ref().try_into().unwrap();
+        Box::new(Sha256HKDFKey(key))
+    }
+
+    fn hmac_sign(&self, key: &crypto::tls13::OkmBlock, message: &[u8]) -> crypto::hmac::Tag {
+        let result = hmac::hmac(hmac::Algorithm::Sha256, key.as_ref(), message, None);
+        crypto::hmac::Tag::new(&result[..])
+    }
+}
+
 
 impl crypto::hmac::Hmac for Sha256Hmac {
     fn with_key(&self, key: &[u8]) -> Box<dyn crypto::hmac::Key> {
@@ -17,6 +53,8 @@ impl crypto::hmac::Hmac for Sha256Hmac {
 }
 
 struct Sha256HmacKey(alloc::vec::Vec<u8>);
+
+struct Sha256HKDFKey([u8; SHA2_256_LEN]);
 
 impl crypto::hmac::Key for Sha256HmacKey {
     fn sign_concat(&self, first: &[u8], middle: &[&[u8]], last: &[u8]) -> crypto::hmac::Tag {
@@ -34,5 +72,21 @@ impl crypto::hmac::Key for Sha256HmacKey {
 
     fn tag_len(&self) -> usize {
         hmac::tag_size(hmac::Algorithm::Sha256)
+    }
+}
+
+impl crypto::tls13::HkdfExpander for Sha256HKDFKey {
+    fn expand_slice(&self, info: &[&[u8]], output: &mut [u8]) -> Result<(), crypto::tls13::OutputLengthError> {
+        hkdf::sha2_256::expand(output, &self.0, &info.concat()).map_err(|_| crypto::tls13::OutputLengthError) 
+    }
+
+    fn expand_block(&self, info: &[&[u8]]) -> crypto::tls13::OkmBlock {
+        let mut okm = [0u8; SHA2_256_LEN];
+        hkdf::sha2_256::expand(&mut okm, &self.0, &info.concat());
+        crypto::tls13::OkmBlock::new(&okm)
+    }
+
+    fn hash_len(&self) -> usize {
+        SHA2_256_LEN
     }
 }
